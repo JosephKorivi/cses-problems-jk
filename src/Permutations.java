f1d1c754090e73cded4dcb443ec0d844c75d7ead
JosephKorivi <joseph.korivi0911@gmail.com>
2025-01-30 22:12:20 +0530

    permutations

--- src/Permutations.java
+++ src/Permutations.java
@@ -31,14 +31,25 @@ public class Permutations {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        if (n <= 2 || n >= 1000000) System.out.println("NO SOLUTION");;
+        if (n == 1) {
+            System.out.println(n);
+            return;
+        }
+        if (n == 2 || n == 3) {
+            System.out.println("NO SOLUTION");
+            return;
+        }
+
+        StringBuilder sb = new StringBuilder();
 
         for (int i=2; i<=n; i+=2) {
-            System.out.print(i + " ");
+            sb.append(i).append(" ");
         }
 
         for (int i=1; i<=n; i+=2) {
-            System.out.print(i + " ");
+            sb.append(i).append(" ");
         }
+
+        System.out.println(sb.toString().trim());
     }
 }
